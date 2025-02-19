09556f50ef1006e9580883d785763b978ddcb083
Emma Robbins <emma.h.sasson@gmail.com>
2025-02-19 14:42:04 -0800

    began playRound function

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 // console.log("Hello, World!")
 
-let getComputerChoice = function () {
-    return Math.floor(Math.random() * 3);
+function getComputerChoice() {
+    const computerNumber = Math.floor(Math.random() * 3);
+    const computerChoice = ['rock', 'paper', 'scissors'];
+
+    return computerChoice[computerNumber];
 }
-getComputerChoice()
 
-const computerChoice = ['rock', 'paper', 'scissors']
 
+function getHumanChoice() {
+    const humanSelect = prompt('Please select Rock, Paper or Scissors');
+    console.log(`You have selected ${humanSelect}`)
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+//while human choice === 'rock'.toUpperCase() || 'paper'.toUpperCase() || 'scissors.toUpperCase()
+
+function playRound(humanChoice, computerChoice) {
+    while (humanChoice.toUpperCase() === 'ROCK' || humanChoice.toUpperCase() === 'PAPER' || humanChoice.toUpperCase() === 'SCISSORS') {
+        if (humanChoice.toUpperCase() === 'ROCK' && computerChoice.toUpperCase() === 'PAPER') {
+            computerScore++;
+            console.log('Computer Wins!')
+        } else if (humanChoice.toUpperCase() === 'ROCK' && computerChoice.toUpperCase() === 'SCISSORS') {
+            humanScore++;
+            console.log('You beat the computer!')
+        } else if ()
+    }
+
+}
