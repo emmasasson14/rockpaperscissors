005c463b55d2b287563707543479d883c1aa4df1
Emma Robbins <emma.h.sasson@gmail.com>
2025-02-19 14:18:14 -0800

    computerchoice function

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!")
+// console.log("Hello, World!")
+
+let getComputerChoice = function () {
+    return Math.floor(Math.random() * 3);
+}
+getComputerChoice()
+
+const computerChoice = ['rock', 'paper', 'scissors']
+
