6fea54a47e1a8e636aef4688a236fe37355e6d63
Emma Robbins <emma.h.sasson@gmail.com>
2025-02-19 20:44:10 -0800

    -finished while loop

--- app.js
+++ app.js
@@ -9,28 +9,50 @@ function getComputerChoice() {
 
 
 function getHumanChoice() {
-    const humanSelect = prompt('Please select Rock, Paper or Scissors');
+    let humanSelect = prompt('Please select rock, paper or scissors');
+    while (
+        humanSelect.toLowerCase() != 'rock' &&
+        humanSelect.toLowerCase() != 'paper' &&
+        humanSelect.toLowerCase() != 'scissors'
+    ) {
+        humanSelect = prompt('Invalid prompt, please try again.');
+    }
+
     console.log(`You have selected ${humanSelect}`)
+    return humanSelect;
+
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    if (toLowerCase(humanChoice) === computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`You chose ${humanChoice} and computer chose ${computerChoice}`)
+        console.log('tie, play again!')
         playRound()
-    } else if (toLowerCase(humanChoice) === 'rock' && computerChoice === 'scissors') {
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log(`You chose ${humanChoice} and computer chose ${computerChoice}`)
         console.log('you win!');
         humanScore++;
-    } else if (toLowerCase(humanChoice) === 'scissors' && computerChoice === 'paper') {
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log(`You chose ${humanChoice} and computer chose ${computerChoice}`)
         console.log('you win!')
         humanScore++;
 
-    } else if (toLowerCase(humanChoice) === 'paper' && computerChoice === 'rock') {
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log(`You chose ${humanChoice} and computer chose ${computerChoice}`)
         console.log('you win!');
         humanScore++;
     } else {
         console.log('computer wins!')
+        console.log(`You chose ${humanChoice} and computer chose ${computerChoice}`)
         computerScore++;
     }
 }
+
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
